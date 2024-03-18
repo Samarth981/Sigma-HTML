3cbf261e26d85c26fc570c8aa34b8cb2a6763578
samarth1056 <samarthpatel981@gmail.com>
2024-03-18 19:31:59 +0530

    update

--- javaScript/1_string_method.js
+++ javaScript/1_string_method.js
@@ -8,3 +8,20 @@ let upper = password.toUpperCase();
 console.log(upper);
 let lower = password.toLowerCase();
 console.log(lower);
+
+//indexOf
+let index = "I love cording"
+let ans = index.indexOf("cording"); // c is coming in 7 number
+console.log(ans);
+
+//slice
+let slice = index.slice(-2) // index.length-2 --> like I -> output->ng
+console.log(slice);
+console.log(`if 2 then come:  ${index.slice(2)}`);
+
+
+//chaining
+console.log(password.trim().toUpperCase()); // 1,2,3 line in code 
+
+let sam = "IloveCoding"
+console.log(sam.slice(-2));
