1f78d778634a8c7822268dc453b81f5b24684892
samarth1056 <samarthpatel981@gmail.com>
2024-03-28 19:09:02 +0530

    updat

--- javaScript/6_object_litrerel.js
+++ javaScript/6_object_litrerel.js
@@ -3,7 +3,7 @@
 const array=["samarth","20","90%",["maths", "physics", "chemestry"]];
 // this is not clerify for what is this
 
-
+// object key is convert as a string
 const object_literel={
     Name: "samarth",
     age:20,
@@ -17,7 +17,36 @@ const twit={
     content: "this is my #firstpsodt",
     likes: 150,
     repost: 5,
-    tags:["@BMU", "@google"]     
+    tags:["@BMU", "@google"]
 }
 
-//how to access a object value
+
+
+//object of oobject
+// creat a individual student info
+const student_Info = {
+    Samarth : {
+        divition: 1,
+        sem: '1st',
+        gard : 'A+',
+        marks: 94,
+    },
+    Parv : {
+        divition: 2,
+        sem: '4st',
+        gard : 'A+',
+        marks: 97,
+    },
+    Raj : {
+        divition: 1,
+        sem: '1st',
+        gard : 'b',
+        marks: 75,
+    },
+    Soham : {
+        divition: 1,
+        sem: '3st',
+        gard : 'b+',
+        marks: 70,
+    },
+}
