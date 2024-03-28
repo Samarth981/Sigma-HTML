20f2da3c3b56ef70a9496c57e15395111ec2cac4
samarth1056 <samarthpatel981@gmail.com>
2024-03-28 18:45:48 +0530

    update

---
+++ javaScript/6_object_litrerel.js
@@ -0,0 +1,23 @@
+// array vs object-literel
+
+const array=["samarth","20","90%",["maths", "physics", "chemestry"]];
+// this is not clerify for what is this
+
+
+const object_literel={
+    Name: "samarth",
+    age:20,
+    marks:90,
+    subjects:["maths", "physics", "chemestry"]
+}; // this is a clerify for what is this
+
+//twiter 
+const twit={
+    userName: "@samarthpatel981",
+    content: "this is my #firstpsodt",
+    likes: 150,
+    repost: 5,
+    tags:["@BMU", "@google"]     
+}
+
+//how to access a object value
